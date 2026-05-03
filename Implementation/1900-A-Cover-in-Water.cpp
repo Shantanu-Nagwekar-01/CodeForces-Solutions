ca623e1b850a84c7f496f797ee6765cddf634c22
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2026-05-03 12:49:13 +0530

    update optimized solution for cover in water

--- Implementation/1900-A-Cover-in-Water.cpp
+++ Implementation/1900-A-Cover-in-Water.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// optimized approach O(n):
 int main()
 {
     int t;
@@ -11,50 +12,27 @@ int main()
         string s;
         cin >> n;
         cin >> s;
-        int l = 0;
-        int total = 0;
-        for (char c : s)
+        int total = 0, threedots = 0;
+        bool found = false;
+        for (int i = 0; i < n ; i++)
         {
-            if (c == '.')
+            if (s[i] == '.')
             {
                 total++;
-            }
-        }
-        if (total == 0)
-        {
-            cout << 0 << endl;
-            continue;
-        }
-        bool threedots = false;
-        for (int r = 2; r < n; r++)
-        {
-            int dot = 0;
-            for (int i = l; i <= r; i++)
-            {
-                if (s[i] == '.')
-                {
-                    dot++;
-                    if (dot == 3)
-                    {
-                        threedots = true;
-                        break;
-                    }
-                }
-                else
-                {
+                threedots++;
+                if (threedots == 3) {
+                    found = true;
                     break;
                 }
             }
-            if (threedots)
+            else
             {
-                break;
+                threedots = 0;
             }
-            l++;
         }
-        if (threedots)
+        if (found)
         {
             cout << 2 << endl;
-            continue;
         }
         else
         {
@@ -62,3 +40,65 @@ int main()
         }
     }
 }
+// first approach
+//  int main()
+//  {
+//      int t;
+//      cin >> t;
+//      while (t--)
+//      {
+//          int n;
+//          string s;
+//          cin >> n;
+//          cin >> s;
+//          int l = 0;
+//          int total = 0;
+//          for (char c : s)
+//          {
+//              if (c == '.')
+//              {
+//                  total++;
+//              }
+//          }
+//          if (total == 0)
+//          {
+//              cout << 0 << endl;
+//              continue;
+//          }
+//          bool threedots = false;
+//          for (int r = 2; r < n; r++)
+//          {
+//              int dot = 0;
+//              for (int i = l; i <= r; i++)
+//              {
+//                  if (s[i] == '.')
+//                  {
+//                      dot++;
+//                      if (dot == 3)
+//                      {
+//                          threedots = true;
+//                          break;
+//                      }
+//                  }
+//                  else
+//                  {
+//                      break;
+//                  }
+//              }
+//              if (threedots)
+//              {
+//                  break;
+//              }
+//              l++;
+//          }
+//          if (threedots)
+//          {
+//              cout << 2 << endl;
+//              continue;
+//          }
+//          else
+//          {
+//              cout << total << endl;
+//          }
+//      }
+//  }
