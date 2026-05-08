2a9093913d153ecfc99d2a492f785709290686dc
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2026-05-08 11:38:10 +0530

    add solution for 1878A

---
+++ Implementation/1878-A-How-Much-Does-Daytona-Cost.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            mp[x]++;
+        }
+        if (mp.count(k) == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+}
