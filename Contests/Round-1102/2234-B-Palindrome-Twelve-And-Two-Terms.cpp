d10956285cc1a31a935acdbe63c7fbca01a7c9f1
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2026-06-08 12:28:23 +0530

    add solution for 2234-B-Palindrome Twelve and Two Terms

--- Contests/Round-1102/2234-B-Palindrome-Twelve-And-Two-Terms.cpp
+++ Contests/Round-1102/2234-B-Palindrome-Twelve-And-Two-Terms.cpp
@@ -11,15 +11,24 @@ int main()
         cin >> n;
         if (n % 12 == 0)
         {
-            cout << 0 << " " << n / 12 << endl;
+            cout << 0 << " " << n << endl;
             continue;
         }
-        if (n % 12 < 10)
+        vector<ll> v(12);
+        for (ll i = 0; i < 12; i++)
         {
-            cout << n % 12 << " " << n - (n % 12) << endl;
-            continue;
+            v[i] = i;
         }
-
+        v[10] = 22;
+        ll a = v[n % 12];
+        if (a <= n)
+        {
+            cout << a << " " << n - a << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
         // 0, 12, ...... n
         //  a + 12x = n
         //  a = n - 12x
