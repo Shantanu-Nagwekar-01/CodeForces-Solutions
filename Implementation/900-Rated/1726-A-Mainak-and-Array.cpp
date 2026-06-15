a87366b78ff6b1376c12f6c76f0752e3b209cb95
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2026-06-15 23:25:40 +0530

    add solution to 1726A Mainak and Array

---
+++ Implementation/900-Rated/1726-A-Mainak-and-Array.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        ll res = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            res = max(res, (a[i] - a[i + 1]));
+        }
+        for (int i = 1; i < n; i++)
+        {
+            res = max(res, a[i] - a[0]);
+        }
+        for (int i = 0; i < n - 1; i++)
+        {
+            res = max(res, a[n - 1] - a[i]);
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
